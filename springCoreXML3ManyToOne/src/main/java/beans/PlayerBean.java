06c01735abfd976d1c58036ec7934184f82f5613
ROHIT  AWARE <126144552+RohitAware-SoftDev@users.noreply.github.com>
2023-04-11 16:24:28 +0530

    Add files via upload

---
+++ springCoreXML3ManyToOne/src/main/java/beans/PlayerBean.java
@@ -0,0 +1,11 @@
+package beans;
+
+import lombok.Data;
+
+@Data
+public class PlayerBean {
+	private int id;
+	private String name;
+	private String role;
+	private int jercy_No;
+}
---
+++ springCoreXML3ManyToOne/src/main/java/beans/TeamBean.java
@@ -0,0 +1,12 @@
+package beans;
+
+import java.util.List;
+
+import lombok.Data;
+@Data
+public class TeamBean {
+	private int id;
+	private String name;
+	private String city;
+	List< PlayerBean> players;
+}
